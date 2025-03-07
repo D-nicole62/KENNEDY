6a0aa3ab37ff09342fab910f4eea43184dfccada
D-nicole62 <148466908+D-nicole62@users.noreply.github.com>
2025-03-07 11:17:37 +0200

    Create partner.java

---
+++ partner.java
@@ -0,0 +1,29 @@
+/*
+ * Copyright (c) 2017. Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+ * Morbi non lorem porttitor neque feugiat blandit. Ut vitae ipsum eget quam lacinia accumsan.
+ * Etiam sed turpis ac ipsum condimentum fringilla. Maecenas magna.
+ * Proin dapibus sapien vel ante. Aliquam erat volutpat. Pellentesque sagittis ligula eget metus.
+ * Vestibulum commodo. Ut rhoncus gravida arcu.
+ */
+
+package org.openlmis.core.domain;
+
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Table(name = "owners")
+
+@Data
+@EqualsAndHashCode(callSuper = false)
+public class Partner extends BaseModel {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+
+    String code;
+    String text;
+}
